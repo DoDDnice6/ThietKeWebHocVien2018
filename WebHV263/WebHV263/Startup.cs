675de2b8e3b700b7d5d6ffbc2f2ef6bf34b0a0e1
dodatdi <30981111+dodatdi@users.noreply.github.com>
2018-04-03 16:09:11 +0700

    3418

---
+++ WebHV263/WebHV263/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebHV263.Startup))]
+namespace WebHV263
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

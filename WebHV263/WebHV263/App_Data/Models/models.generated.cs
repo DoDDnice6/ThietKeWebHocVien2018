994ee7842d18055a30fcb5440a3d49148cca74a9
dodatdi <30981111+dodatdi@users.noreply.github.com>
2018-04-18 15:15:52 +0700

    xxx

--- WebHV263/WebHV263/App_Data/Models/models.generated.cs
+++ WebHV263/WebHV263/App_Data/Models/models.generated.cs
@@ -19,8 +19,8 @@ using Umbraco.ModelsBuilder;
 using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "59ddf224c02eb107")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.10")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "1de48f6ed4e2cfff")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
@@ -1830,32 +1830,6 @@ namespace Umbraco.Web.PublishedContentModels
 		public string TieuDe
 		{
 			get { return this.GetPropertyValue<string>("tieuDe"); }
-		}
-	}
-
-	/// <summary>ádsd</summary>
-	[PublishedContentModel("adsd")]
-	public partial class Adsd : Page
-	{
-#pragma warning disable 0109 // new is redundant
-		public new const string ModelTypeAlias = "adsd";
-		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
-#pragma warning restore 0109
-
-		public Adsd(IPublishedContent content)
-			: base(content)
-		{ }
-
-#pragma warning disable 0109 // new is redundant
-		public new static PublishedContentType GetModelContentType()
-		{
-			return PublishedContentType.Get(ModelItemType, ModelTypeAlias);
-		}
-#pragma warning restore 0109
-
-		public static PublishedPropertyType GetModelPropertyType<TValue>(Expression<Func<Adsd, TValue>> selector)
-		{
-			return PublishedContentModelUtility.GetModelPropertyType(GetModelContentType(), selector);
 		}
 	}
 
